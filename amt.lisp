2cd4e9c133537275b6ef3dedd8a324ed4230b871
Takeru Ohta <phjgt308@gmail.com>
2010-09-27 11:52:04 +0900

    ノードのアロケータを作成(とりあえずグローバルで)

--- amt.lisp
+++ amt.lisp
@@ -7,9 +7,11 @@
            (bitmap-length start end))
   (logcount (ldb (byte (- end start) start) bitmap)))
 
+(defparameter *empty-entries* #()) ; XXX:
+
 (defstruct amt-node
   (bitmap  0   :type bitmap)
-  (entries #() :type simple-vector))
+  (entries *empty-entries* :type simple-vector))
 
 (defun valid-entry-p (node arc)
   (declare (amt-node node)
@@ -24,16 +26,25 @@
         nil
       (aref entries (ctpop bitmap :end arc)))))
 
+(defparameter *entry-pool* (make-array (1+ +BITMAP-SIZE+) :initial-element nil))
+
 (defun set-entry (node arc new-entry)
   (declare #.*fastest*
            (amt-node node)
            (arc arc))
   (with-slots (bitmap entries) node
+
     (let ((new-entry-index (ctpop bitmap :end arc)))
       (unless (valid-entry-p node arc)
-        (setf (ldb (byte 1 arc) bitmap) 1                          
-              entries (adjust-array entries (1+ (length entries))))
-        (loop FOR i FROM (1- (length entries)) DOWNTO (1+ new-entry-index)
-          DO
-          (setf (aref entries i) (aref entries (1- i)))))
-      (setf (aref entries new-entry-index) new-entry)))) 
+        (unless #1=(aref (the simple-vector *entry-pool*) (1+ (length entries)))
+          (push (make-array (1+ (length entries))) #1#))
+        (let ((new-entries (pop #1#)))
+          (declare (simple-vector new-entries))
+          (setf (ldb (byte 1 arc) bitmap) 1)
+          (replace new-entries entries :end1 new-entry-index :end2 new-entry-index)
+          (replace new-entries entries :start1 (1+ new-entry-index) :start2 new-entry-index)
+          (when (plusp (length entries))
+            (push entries (aref (the simple-vector *entry-pool*) (length entries))))
+          (setf entries new-entries)))
+      (setf (aref entries new-entry-index) new-entry))))
+
